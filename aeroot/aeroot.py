c4da2fcee36820047895fc820cc2766cd5d8b7be
eleguevel <eleguevel@quarkslab.com>
2021-06-13 11:22:46 +0200

    Handle AmbiguousProcessNameError

--- aeroot/aeroot.py
+++ aeroot/aeroot.py
@@ -4,7 +4,7 @@ AERoot main module
 
 from enum import IntEnum, auto
 
-from aeroot.avd import Avd, AVDError
+from aeroot.avd import Avd, AVDError, AmbiguousProcessNameError
 
 
 class ProcessNotRunningError(Exception): pass
@@ -37,6 +37,12 @@ class AERoot:
             self.avd.selinux_setenforce(0)
         except AVDError as err:
             raise AERootError(err)
+        except AmbiguousProcessNameError:
+            msg = (
+                "Several processes with the same name are currently running",
+                "You should use the pid to target the process"
+            )
+            raise AERootError("\n".join(msg))
 
 
     def cleanup(self):
